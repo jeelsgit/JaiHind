dadb5a88c75c41ba8924c8c140e38058500b193d
mmb0710 <mmb0702004@gmail.com>
2024-02-07 16:24:17 -0500

    Branch 1 Meet Maheta

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.college.converter;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate() start");
 
         Button buttonConvert = findViewById(R.id.convertButton);
 
